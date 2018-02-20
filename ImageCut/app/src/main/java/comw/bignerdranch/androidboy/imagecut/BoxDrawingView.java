2a2b4c5be3f96dd8eea1cd2b44f7dbfa1eec9563
messi-wpy <904315105@qq.com>
2018-02-21 00:45:49 +0800

    新手任务触摸事件

--- ImageCut/app/src/main/java/comw/bignerdranch/androidboy/imagecut/BoxDrawingView.java
+++ ImageCut/app/src/main/java/comw/bignerdranch/androidboy/imagecut/BoxDrawingView.java
@@ -11,6 +11,7 @@ import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.net.Uri;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -32,6 +33,14 @@ public class BoxDrawingView extends View {
     private float imageWidth;
     private float imageHeight;
     private float radius;
+    private double lastDis;
+    private double currentDis;
+    private float scaleDate=1;
+    private float xMove;
+    private float yMove;
+    private float scale;
+    private float centerX;
+    private float centerY;
      private Paint paint=new Paint();
      private  String STATUS="INIT";
     public BoxDrawingView(Context context){
@@ -57,6 +66,10 @@ public class BoxDrawingView extends View {
                 canvas.drawBitmap(mBitmap,mMatrix,null);
                 addCircle(canvas,paint);
                 break;
+            case "SCALE":
+                canvas.drawBitmap(mBitmap,mMatrix,null);
+                addCircle(canvas,paint);
+                break;
 
 
 
@@ -71,33 +84,64 @@ public class BoxDrawingView extends View {
             // 分别获取到ZoomImageView的宽度和高度
             width = getWidth();
             height = getHeight();
-            currentX=width/2;
-            currentY=height/2;
+            centerX=width/2;
+            centerY=height/2;
         }
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getActionMasked()) {
-            case MotionEvent.ACTION_MOVE:
-                if(event.getPointerCount()==1){
-                    STATUS="MOVE";
-                    if(lastX==-1&&lastY==-1){
-                        lastX=event.getX();
-                        lastY=event.getY();
-                    }
-                    currentY=event.getY();
-                    currentX=event.getX();
-                    float xMove=currentX-lastX;
-                    float yMove=currentY-lastY;
+            case MotionEvent.ACTION_DOWN://一个手指按压时出来
+                lastX=event.getX();
+                lastY=event.getY();
+                Log.i(TAG, "onTouchEvent: down");
+                break;
 
-                    lastX=event.getX();
-                    lastY=event.getY();
+            case MotionEvent.ACTION_POINTER_DOWN://两个手指按压时才出来
+                if(event.getPointerCount()==2){
+                    lastDis=distanceBetweenFingers(event);
+                }
+                Log.i(TAG, "onTouchEvent: pointDown");
+                break;
+            case MotionEvent.ACTION_MOVE:
+                Log.i(TAG, "onTouchEvent: move");
+                if(event.getPointerCount()==1) {
+                    STATUS = "MOVE";
+                    currentY = event.getY();
+                    currentX = event.getX();
+                    xMove = currentX - lastX;
+                    yMove = currentY - lastY;
+                   centerX+=xMove;
+                   centerY+=yMove;
+                    lastX = event.getX();
+                    lastY = event.getY();
+
+                    /*    if (centerY>=(height/2-((imageHeight*scale*scaleDate)/2-radius))&&
+                                centerY<=height/2+((imageHeight*scale*scaleDate)/2-radius)) {
+                            mMatrix.postTranslate(0, yMove);
+                        }
+                        if (centerX>=(width/2-((imageWidth*scaleDate*scale)/2-radius))&&
+                                centerX<=width/2+((imageWidth*scale*scaleDate)/2-radius)){
+                            mMatrix.postTranslate(xMove,0);
+                        }*/
 
                     mMatrix.postTranslate(xMove,yMove);
+                        invalidate();
+                        break;
+
+                    }
+
+                //表示两个手指是放缩，
+                else {
+
+                       currentDis = distanceBetweenFingers(event);
+                       scaleDate = (float) (currentDis / lastDis);
+                       lastDis = currentDis;
+                       mMatrix.postScale(scaleDate, scaleDate);
+                    STATUS="SCALE";
                     invalidate();
                     break;
-
                 }
 
         }
@@ -112,7 +156,8 @@ public class BoxDrawingView extends View {
         imageWidth=mBitmap.getWidth();
         if (imageWidth>width||imageHeight>height){
             if (imageHeight-height>imageWidth-width){
-                float scale=height/(imageHeight*1.5f);
+
+                scale=height/(imageHeight*1.3f);
                 mMatrix.postScale(scale,scale);
                 float translateX=(width-(imageWidth*scale))/2;
                 float translateY=(height-(imageHeight*scale))/2;
@@ -121,7 +166,7 @@ public class BoxDrawingView extends View {
                 imageHeight=imageHeight*scale;
             }
             else {
-                float scale=width/(imageWidth*1.5f);
+                float scale=width/(imageWidth*1.3f);
                 mMatrix.postScale(scale,scale);
                 float translateY=(height-(imageHeight*scale))/2;
                 float translateX=(width-(imageWidth*scale))/2;
@@ -165,6 +210,12 @@ public class BoxDrawingView extends View {
         addCircle(canvas,paint);
       //  paint.setXfermode(null);
 
+    }
+
+    private double distanceBetweenFingers(MotionEvent event) {
+        float disX = Math.abs(event.getX(0) - event.getX(1));
+        float disY = Math.abs(event.getY(0) - event.getY(1));
+        return Math.sqrt(disX * disX + disY * disY);
     }
 
 
